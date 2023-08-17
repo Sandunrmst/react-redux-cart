18eddbd2edc036f95280959f6f23e820960498b9
sandunrmst <sandunalugc@gmail.com>
2023-08-17 12:18:05 +0530

    update total and cart items

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,20 @@ function App() {
   const dispatch = useDispatch()
 
   console.log(cart)
+
+  let Total = 0;
+  if(Array.isArray(cart)){
+    cart.forEach((ele)=>{
+      Total = Total + ele.count * ele.price;
+    }) 
+  
+  }
+
+  //cart count 
+  let cartCount = 0
+  cart.forEach((ele)=> {
+    cartCount = cartCount + ele.count;
+  })
   return (
     <>
     <h1>Product List</h1>
@@ -17,8 +31,8 @@ function App() {
      <p key={id}> {price} | {cpu} | {ram} <button onClick={()=> dispatch(addItemToCart(id,price,cpu,ram))}>Add to Cart</button></p>)}
 
      <h1>Cart</h1>
-     <h3>{cart.length}</h3>
-     <h3>Total : $ 0</h3>
+     <h3>{cartCount}</h3>
+     <h3>Total : Rs {Total}</h3>
     </>
   )
 }
